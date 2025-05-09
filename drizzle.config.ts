2e953bda2bcd5e5b07577f41909c9210aaa397c0
Anh Mac <anh.mac@zozo.com>
2025-05-09 16:05:47 +0700

    feat: generate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'sqlite',
-  schema: './layer/base/server/database/schema.ts',
-  out: './layer/base/server/database/migrations'
+  schema: './layers/database/server/database/schema.ts',
+  out: './layers/database/server/database/migrations'
 })
