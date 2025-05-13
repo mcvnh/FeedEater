da0214371b28bdd20318ae75b2f986a41f1c63fa
Anh Mac <anh.mac@zozo.com>
2025-05-13 15:36:29 +0700

    chore: add debug info

--- layers/base/server/plugins/hub.ts
+++ layers/base/server/plugins/hub.ts
@@ -4,8 +4,10 @@ import yaml from 'js-yaml'
 
 export default defineNitroPlugin(() => {
   hubHooks.hook('bindings:ready', async () => {
+    console.log('Loading feeds from feeds.yml')
     const feeds = yaml.load(fs.readFileSync('feeds.yml', 'utf8')) as {name: string, url: string}[]
 
+    console.log('Feeds:', feeds)
     const feedNames = feeds.map((feed) => feed.name)
     const existingFeeds = await useDrizzle().select().from(tables.feeds).all()
 
@@ -24,6 +26,9 @@ export default defineNitroPlugin(() => {
       console.log('Inserted feeds:', newFeeds)
     }
 
+    console.log('Updating feeds...')
     await runTask('articles:update')
+
+    console.log('Completing feeds update')
   })
 })
