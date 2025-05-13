986acb2f38edd9f1e11f6be5251e091128b923b7
Anh Mac <anh.mac@zozo.com>
2025-05-13 16:24:52 +0700

    feat: update

--- layers/base/server/plugins/hub.ts
+++ layers/base/server/plugins/hub.ts
@@ -4,26 +4,33 @@ import yaml from 'js-yaml'
 
 export default defineNitroPlugin(() => {
   onHubReady(async () => {
-    const feeds = yaml.load(fs.readFileSync('feeds.yml', 'utf8')) as {name: string, url: string}[]
+    try {
+      const feeds = yaml.load(fs.readFileSync('feeds.yml', 'utf8')) as {name: string, url: string}[]
 
-    const feedNames = feeds.map((feed) => feed.name)
-    const existingFeeds = await useDrizzle().select().from(tables.feeds).all()
+      const feedNames = feeds.map((feed) => feed.name)
+      const existingFeeds = await useDrizzle().select().from(tables.feeds).all()
+      await hubKV().set('feeds', JSON.stringify(feeds))
 
-    const shouldDeleteFeeds = existingFeeds.filter((feed) => !feedNames.includes(feed.name))
-    if (shouldDeleteFeeds.length > 0) {
-      await useDrizzle().delete(tables.feeds).where(inArray(tables.feeds.name, shouldDeleteFeeds.map((feed) => feed.name))).execute()
+      const shouldDeleteFeeds = existingFeeds.filter((feed) => !feedNames.includes(feed.name))
+      if (shouldDeleteFeeds.length > 0) {
+        await useDrizzle().delete(tables.feeds).where(inArray(tables.feeds.name, shouldDeleteFeeds.map((feed) => feed.name))).execute()
+      }
+
+      const allNames = existingFeeds.map((feed) => feed.name)
+      const newFeeds = feeds.filter((feed) => !allNames.includes(feed.name))
+      if (newFeeds.length > 0) {
+        const insertFeeds = newFeeds.map((feed) => ({
+          name: feed.name,
+          url: feed.url,
+        }))
+        await hubKV().set('insert feeds', JSON.stringify(feeds))
+        await useDrizzle().insert(tables.feeds).values(insertFeeds).execute()
+      }
+
+      await runTask('articles:update')
+    } catch (error) {
+      console.error('Error loading feeds:', error)
+      await hubKV().set('error', JSON.stringify(error))
     }
-
-    const allNames = existingFeeds.map((feed) => feed.name)
-    const newFeeds = feeds.filter((feed) => !allNames.includes(feed.name))
-    if (newFeeds.length > 0) {
-      const insertFeeds = newFeeds.map((feed) => ({
-        name: feed.name,
-        url: feed.url,
-      }))
-      await useDrizzle().insert(tables.feeds).values(insertFeeds).execute()
-    }
-
-    await runTask('articles:update')
   })
 })
