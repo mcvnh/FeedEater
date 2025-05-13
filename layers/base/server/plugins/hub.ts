19e3e8398fb4a0d8707b73ac8aa632f1bdfbdb34
Anh Mac <anh.mac@zozo.com>
2025-05-13 15:25:47 +0700

    test: hub

--- layers/base/server/plugins/hub.ts
+++ layers/base/server/plugins/hub.ts
@@ -1,9 +1,29 @@
+import { inArray } from 'drizzle-orm'
 import fs from 'fs'
 import yaml from 'js-yaml'
 
 export default defineNitroPlugin(() => {
-  hubHooks.hook('bindings:ready', () => {
-    const docs = yaml.load(fs.readFileSync('feeds.yml', 'utf8'))
-    console.log(docs)
+  hubHooks.hook('bindings:ready', async () => {
+    const feeds = yaml.load(fs.readFileSync('feeds.yml', 'utf8')) as {name: string, url: string}[]
+
+    const feedNames = feeds.map((feed) => feed.name)
+    const existingFeeds = await useDrizzle().select().from(tables.feeds).all()
+
+    const shouldDeleteFeeds = existingFeeds.filter((feed) => !feedNames.includes(feed.name))
+    if (shouldDeleteFeeds.length > 0) {
+      await useDrizzle().delete(tables.feeds).where(inArray(tables.feeds.name, shouldDeleteFeeds.map((feed) => feed.name))).execute()
+      console.log('Deleted feeds:', shouldDeleteFeeds)
+    }
+
+    const newFeeds = feeds.filter((feed) => !existingFeeds.map((f) => f.name).includes(feed.name))
+    if (newFeeds.length > 0) {
+      await useDrizzle().insert(tables.feeds).values(newFeeds.map((feed) => ({
+        name: feed.name,
+        url: feed.url,
+      }))).execute()
+      console.log('Inserted feeds:', newFeeds)
+    }
+
+    await runTask('articles:update')
   })
 })
